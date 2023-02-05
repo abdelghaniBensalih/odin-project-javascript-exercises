6df259d690b4c607d19183da06ae6a2bda96b07c
Abdelghani Bensalih <abdelghaniben40@gmail.com>
2023-02-05 22:33:28 +0100

    commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    var expression=word
+    if (num==0){
+        var expression="";
+        return expression
+    }
+    else if (num<0){
+        var expression="ERROR";
+        return expression
+    }
+    else{
+        for (i=1; i<num;i++){
+    
+            expression = expression + word
+        
+        }
+        return expression
+    }
+    
 };
 
 // Do not edit below this line
