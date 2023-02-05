f374a93b944b289fa15efb9abf53088684f7b72d
Abdelghani Bensalih <abdelghaniben40@gmail.com>
2023-02-05 22:35:57 +0100

    commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let myArray=args[0],
+        newArray=[],
+        myFunct = function(item){
+            if (!args.includes(item)){
+                newArray.push(item)
+            }
+        };
+    myArray.forEach(myFunct)
+    return newArray
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
